6f96572d5678626264d7f43da8d404039e3a71e5
Jason Young <{ID}+61697550+Gruffy20@users.noreply.github.com>
2023-02-27 11:02:54 +0000

    Removed dark mode, changed color scheme

--- script.js
+++ script.js
@@ -11,9 +11,20 @@ function myFunction() {
     $(this).toggleClass("down");
 })
 
-function darkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  }
+// function darkMode() {
+//     var element = document.body;
+//     element.classList.toggle("dark-mode");
+//   }
 
-  document.getElementById("mode-btn").addEventListener("click", darkMode);
+//   document.getElementById("mode-btn").addEventListener("click", darkMode);
+
+
+// let darkToggle = document.querySelector('#toggle');
+
+// darkToggle.addEventListener('change', ()=> {
+//   document.body.classList.toggle('dark');
+// })
+
+// document.getElementById("darkToggle").addEventListener("click", function(){
+//     document.getElementsByTagName('nav')[0].classList.toggle("dark");
+// });
